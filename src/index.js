62a1d5ff754ead08bca79927d94009304f51b74b
Gabriel Fontes <hi@m7.rs>
2023-05-21 16:40:00 -0300

    adicionar syntax highlighting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import Reveal from 'reveal.js'
-import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.js'
-import 'reveal.js/css/reveal.scss'
-import 'gelos-revealjs/scss/gelos.scss'
+import Reveal          from 'reveal.js';
+import RevealMarkdown  from 'reveal.js/plugin/markdown/markdown.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js';
+import revealScss      from 'reveal.js/css/reveal.scss';
+
 import gelosLogo from 'gelos-revealjs/svg/logo.svg';
+import gelosScss from 'gelos-revealjs/scss/gelos.scss';
+
 
 document.getElementById("gelos-logo").innerHTML = gelosLogo;
 
 Reveal.initialize({
-    plugins: [RevealMarkdown],
+    plugins: [RevealMarkdown, RevealHighlight],
     slideNumber: true,
 });
