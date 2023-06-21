ec4822f8b2cb34e295ec202d9e0c70d185ac9e67
Gabriel Fontes <hi@m7.rs>
2023-06-21 09:47:49 -0300

    adicionar opção de assets estáticos

--- .webpack.js
+++ .webpack.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,5 +29,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
   },
-  plugins: [new HtmlWebpackPlugin({template: 'src/index.html'})],
+  plugins: [
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
+      new CopyWebpackPlugin({ patterns: [{ from: 'static' }] }),
+  ],
 };
