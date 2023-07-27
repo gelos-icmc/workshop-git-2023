fcb0f0f8bd753dcc1fb01f71b3ef057cfc9ad97b
Gabriel Fontes <hi@m7.rs>
2023-07-27 00:32:04 -0300

    formatar flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,53 @@
 {
   nixConfig = {
-    extra-substituters = [ "https://cache.m7.rs" ];
-    extra-trusted-public-keys = [ "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg=" ];
+    extra-substituters = ["https://cache.m7.rs"];
+    extra-trusted-public-keys = ["cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg="];
   };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs.lib) genAttrs getExe importJSON readFile;
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forEachSystem = f: genAttrs systems (s: f s nixpkgs.legacyPackages.${s});
-      mkApp = package: { type = "app"; program = getExe package; };
-    in
-    rec {
-      # Pacotes disponíveis via "nix build", "nix shell", "nix run"...
-      packages = forEachSystem (_: pkgs: rec {
-        # O site em si
-        default = site;
-        site = pkgs.buildNpmPackage {
-          inherit (importJSON ./package.json) pname version;
-          src = ./.;
-          installPhase = "cp -r dist $out";
-          npmDepsHash = readFile ./.deps-hash;
-          makeCacheWritable = true;
-        };
-        # Servir o site com o webfsd
-        serve = pkgs.writeShellScriptBin "serve" ''
-          echo "Serving on http://localhost:8080"
-          ${getExe pkgs.webfs} -F -r ${site} -f index.html -p 8080
-        '';
-        # Rodar prefetch-npm-deps e atualizar o hash da package-lock
-        fix-hash = pkgs.writeShellScriptBin "fix-hash" ''
-          ${getExe pkgs.prefetch-npm-deps} package-lock.json > .deps-hash
-        '';
-      });
-
-      # Rodar o serve quando usarmos "nix run"
-      apps = forEachSystem (sys: _: {
-        default = mkApp packages.${sys}.serve;
-      });
-
-      # Formatador ao usar "nix fmt"
-      formatter = forEachSystem (_: pkgs: pkgs.nixpkgs-fmt);
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs.lib) genAttrs getExe importJSON readFile;
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forEachSystem = f: genAttrs systems (s: f s nixpkgs.legacyPackages.${s});
+    mkApp = package: {
+      type = "app";
+      program = getExe package;
     };
-}
+  in rec {
+    # Pacotes disponíveis via "nix build", "nix shell", "nix run"...
+    packages = forEachSystem (_: pkgs: rec {
+      # O site em si
+      default = site;
+      site = pkgs.buildNpmPackage {
+        inherit (importJSON ./package.json) pname version;
+        src = ./.;
+        installPhase = "cp -r dist $out";
+        npmDepsHash = readFile ./.deps-hash;
+        makeCacheWritable = true;
+      };
+      # Servir o site com o webfsd
+      serve = pkgs.writeShellScriptBin "serve" ''
+        echo "Serving on http://localhost:8080"
+        ${getExe pkgs.webfs} -F -r ${site} -f index.html -p 8080
+      '';
+      # Rodar prefetch-npm-deps e atualizar o hash da package-lock
+      fix-hash = pkgs.writeShellScriptBin "fix-hash" ''
+        ${getExe pkgs.prefetch-npm-deps} package-lock.json > .deps-hash
+      '';
+    });
 
+    # Rodar o serve quando usarmos "nix run"
+    apps = forEachSystem (sys: _: {
+      default = mkApp packages.${sys}.serve;
+    });
+
+    # Formatador ao usar "nix fmt"
+    formatter = forEachSystem (_: pkgs: pkgs.alejandra);
+  };
+}
