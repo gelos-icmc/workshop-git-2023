12f081b4346f8933fb2e764df20c1b0a11931ef3
Gabriel Fontes <hi@m7.rs>
2023-07-29 16:54:32 -0300

    adicionar css custom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Reveal          from 'reveal.js';
 import RevealMarkdown  from 'reveal.js/plugin/markdown/markdown.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js';
-import revealScss      from 'reveal.js/css/reveal.scss';
+import 'reveal.js/css/reveal.scss';
 
 import gelosLogo from 'gelos-revealjs/svg/logo.svg';
-import gelosScss from 'gelos-revealjs/scss/gelos.scss';
+import 'gelos-revealjs/scss/gelos.scss';
+
+import './style.css';
 
 document.getElementById("gelos-logo").innerHTML = gelosLogo;
 
