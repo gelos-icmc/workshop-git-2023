97230101e9ac4f8776dbe4b262b036c57d0f69d3
Gabriel Fontes <hi@m7.rs>
2024-01-17 11:24:50 -0300

    bump theme

--- flake.nix
+++ flake.nix
@@ -5,30 +5,16 @@
 
     gelos-theme.url = "github:gelos-icmc/beamer-theme";
     gelos-theme.inputs.nixpkgs.follows = "nixpkgs";
-    gelos-theme.inputs.systems.follows = "systems";
   };
 
-  outputs = {
-    self,
-    systems,
-    nixpkgs,
-    gelos-theme,
-  }: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
+  outputs = { systems, nixpkgs, gelos-theme, ...}: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
-    packages = eachSystem (system: pkgs: let
-      texlive = gelos-theme.packages.${system}.texlive-env;
-    in {
-      default = pkgs.stdenvNoCC.mkDerivation {
+    packages = eachSystem (system: {
+      default = gelos-theme.packages.${system}.mkGelosSlides {
         pname = "workshop-git";
-        version = self.shortRev or "dirty";
         src = ./.;
-        buildInputs = [texlive pkgs.pandoc];
-        buildPhase = "pandoc -t beamer workshop.md -o workshop.pdf";
-        installPhase = "install -D *.pdf -t $out";
       };
     });
-    formatter = eachSystem (system: pkgs: pkgs.alejandra);
   };
 }
